dd93e42321be336a697c4aa94a00256c34434606
alseambusher <alseambusher@gmail.com>
2016-12-22 16:07:20 +0530

    schedule consumer added

--- src/lib/schedules.js
+++ src/lib/schedules.js
@@ -10,6 +10,8 @@ export class Schedules {
       filename: filename,
       lockTime: lockTime,
       unlockTime: unlockTime,
+      locked: false,
+      done: false,
       timestamp: new Date().toString()
     };
     return this.db.insert(data);
@@ -25,6 +27,14 @@ export class Schedules {
     this.db.find({ _id: _id }, function (err, docs) {
       callback(docs[0]);
     });
+  }
+
+  done(_id) {
+    this.db.update({ _id: _id}, { $set: {done: true}});
+  }
+
+  lock(_id) {
+    this.db.update({ _id: _id}, { $set: {locked: true}});
   }
 
   reload() {
--- src/tartarus-process.js
+++ src/tartarus-process.js
@@ -1,10 +1,14 @@
 /*jshint esversion: 6 */
 import * as tartarus from "./lib/tartarus";
-import {default as decrypt} from "./lib/encrypt";
+import {default as crypt} from "./lib/encrypt";
 
 var processing = [];
+var s_processing = [];
 
 function run() {
+  console.log("processsing", processing);
+  console.log("s_processsing", s_processing);
+
   tartarus.files.files((files) => {
     files.forEach((file) => {
       if (!file.done && (new Date(file.unlockTime).getTime() <= new Date().getTime())){
@@ -12,7 +16,8 @@ function run() {
           processing.push(file._id);
           console.log("decrypting " + file.filename);
           tartarus.settings.key((password) => {
-            decrypt(file.filename, password, () => {
+            // decrypt
+            crypt(file.filename, password, () => {
               var index = processing.indexOf(file._id);
               if (index > -1) {
                 processing.splice(index, 1);
@@ -21,6 +26,50 @@ function run() {
               console.log("decrypted " + file.filename);
             }, true);
           });
+        }
+      }
+    });
+  });
+
+  tartarus.schedules.files((files) => {
+    files.forEach((file) => {
+      // if not done and past lock time
+      if(!file.done && (new Date(file.lockTime).getTime() <= new Date().getTime())) {
+        // if it is not locked then encrypt it and lock it
+        if(!file.locked){
+          if (s_processing.indexOf(file._id) <= -1) {
+            s_processing.push(file._id);
+            console.log("encrypting " + file.filename);
+            tartarus.settings.key((password) => {
+              // encrypt
+              crypt(file.filename, password, () => {
+                var index = s_processing.indexOf(file._id);
+                if (index > -1) {
+                  s_processing.splice(index, 1);
+                }
+                tartarus.schedules.lock(file._id);
+                console.log("encrypted " + file.filename);
+              }, false);
+            });
+          }
+        }
+        // if file is locked then unlock it if it is past unlock time and mark done
+        else if ((new Date(file.unlockTime).getTime() <= new Date().getTime())) {
+          if (s_processing.indexOf(file._id) <= -1) {
+            s_processing.push(file._id);
+            console.log("decrypting " + file.filename);
+            tartarus.settings.key((password) => {
+              // encrypt
+              crypt(file.filename, password, () => {
+                var index = s_processing.indexOf(file._id);
+                if (index > -1) {
+                  s_processing.splice(index, 1);
+                }
+                tartarus.schedules.done(file._id);
+                console.log("decrypting " + file.filename);
+              }, true);
+            });
+          }
         }
       }
     });
